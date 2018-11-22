092c8236f279ba519cd77923d3ae64af4b5de1da
Alessandro Marzoli <a.marzoli@ambrogio.com>
2018-11-22 16:41:41 +0100

    indent

--- Sources/EventBus/EventBus.swift
+++ Sources/EventBus/EventBus.swift
@@ -85,13 +85,13 @@ extension EventBus {
     self.knownTypes[identifier] = String(describing: eventType)
   }
 
-//  @inline(__always)
-//  fileprivate func updateChains<T>(for eventType: T.Type, closure: (inout NSHashTable<AnyObject>) -> ()) {
-//    let identifier = ObjectIdentifier(eventType)
-//    let chained = self.chained[identifier] ?? []
-//    self.chained[identifier] = self.update(set: chained, closure: closure)
-//    self.knownTypes[identifier] = String(describing: eventType)
-//  }
+  //  @inline(__always)
+  //  fileprivate func updateChains<T>(for eventType: T.Type, closure: (inout NSHashTable<AnyObject>) -> ()) {
+  //    let identifier = ObjectIdentifier(eventType)
+  //    let chained = self.chained[identifier] ?? []
+  //    self.chained[identifier] = self.update(set: chained, closure: closure)
+  //    self.knownTypes[identifier] = String(describing: eventType)
+  //  }
 
   @inline(__always)
   private func update(set: SubscriberSet, closure: (inout SubscriberSet) -> ()) -> SubscriberSet? {
@@ -120,19 +120,20 @@ extension EventBus {
 
   @discardableResult
   public func add<T>(subscriber: T, for eventType: T.Type, queue: DispatchQueue, options: Options? = .none) -> SubscriptionCancellable {
-//    self.warnIfNonClass(subscriber)
-//    if options.contains(.warnUnknown) {
-//      self.warnIfUnknown(eventType)
-//    }
-//    self.lock.with {
+    //    self.warnIfNonClass(subscriber)
+    //    if options.contains(.warnUnknown) {
+    //      self.warnIfUnknown(eventType)
+    //    }
+    //    self.lock.with {
 
     let subscription = Subscription<AnyObject>(subscriber: subscriber as AnyObject, queue: queue, cancellationClosure: { [weak self] completion in
       guard let self = self else { return }
+
       self.remove(subscriber: subscriber, for: eventType, options: options)
     })
 
-      updateSubscribers(for: eventType) { subscribed in
-        subscribed.insert(subscription)
+    updateSubscribers(for: eventType) { subscribed in
+      subscribed.insert(subscription)
     }
 
     return subscription.token
@@ -141,56 +142,56 @@ extension EventBus {
 
   /// Removes a subscriber from a given event type.
   public func remove<T>(subscriber: T, for eventType: T.Type, options: Options? = .none) {
-//    self.warnIfNonClass(subscriber)
-//    if options.contains(.warnUnknown) {
-//      self.warnIfUnknown(eventType)
-//    }
-//    self.lock.with {
-      updateSubscribers(for: eventType) { subscribed in
-//        subscribed.remove(subscriber as AnyObject)
-        while let index = subscribed.index(where: { $0 == subscriber as AnyObject }) {
-          subscribed.remove(at: index)
-        }
+    //    self.warnIfNonClass(subscriber)
+    //    if options.contains(.warnUnknown) {
+    //      self.warnIfUnknown(eventType)
+    //    }
+    //    self.lock.with {
+    updateSubscribers(for: eventType) { subscribed in
+      //        subscribed.remove(subscriber as AnyObject)
+      while let index = subscribed.index(where: { $0 == subscriber as AnyObject }) {
+        subscribed.remove(at: index)
       }
-//    }
+    }
+    //    }
   }
 
   /// Removes a subscriber from all its subscriptions.
   public func remove<T>(subscriber: T, options: Options? = .none) {
-//    self.warnIfNonClass(subscriber)
-//    self.lock.with {
-      for (identifier, subscribed) in self.subscribed {
-        self.subscribed[identifier] = self.update(set: subscribed) { subscribed in
-          //subscribed.remove(subscriber as AnyObject)
-          while let index = subscribed.index(where: { $0 == subscriber as AnyObject }) {
-            subscribed.remove(at: index)
-          }
+    //    self.warnIfNonClass(subscriber)
+    //    self.lock.with {
+    for (identifier, subscribed) in self.subscribed {
+      self.subscribed[identifier] = self.update(set: subscribed) { subscribed in
+        //subscribed.remove(subscriber as AnyObject)
+        while let index = subscribed.index(where: { $0 == subscriber as AnyObject }) {
+          subscribed.remove(at: index)
         }
       }
-//    }
+    }
+    //    }
   }
 
   public func removeAllSubscribers() {
     self.subscribed.removeAll()
-//    self.lock.with {
-//      self.subscribed = [:]
-//    }
+    //    self.lock.with {
+    //      self.subscribed = [:]
+    //    }
   }
 
   internal func hasSubscriber<T>(_ subscriber: T, for eventType: T.Type, options: Options? = .none) -> Bool {
-//    self.warnIfNonClass(subscriber)
-//    if options.contains(.warnUnknown) {
-//      self.warnIfUnknown(eventType)
-//    }
-//    return self.lock.with {
-      guard let subscribed = self.subscribed[ObjectIdentifier(eventType)] else {
-        return false
-      }
+    //    self.warnIfNonClass(subscriber)
+    //    if options.contains(.warnUnknown) {
+    //      self.warnIfUnknown(eventType)
+    //    }
+    //    return self.lock.with {
+    guard let subscribed = self.subscribed[ObjectIdentifier(eventType)] else {
+      return false
+    }
     let subscriptions = subscribed.filter { $0.underlyngObject === subscriber as AnyObject }
 
     assert(subscriptions.isEmpty || subscriptions.count == 1, "TODO ---> \(subscriptions)")
     return subscriptions.isEmpty || subscriptions.count == 1
-//    }
+    //    }
   }
 }
 
@@ -198,34 +199,34 @@ extension EventBus {
   
   @discardableResult
   public func notify<T>(_ eventType: T.Type, options: Options? = .none, closure: @escaping (T) -> ()) -> Bool {
-//    if options.contains(.warnUnknown) {
-//      self.warnIfUnknown(eventType)
-//    }
-//    self.logEvent(eventType)
+    //    if options.contains(.warnUnknown) {
+    //      self.warnIfUnknown(eventType)
+    //    }
+    //    self.logEvent(eventType)
     //return self.lock.with {
-      var handledNotifications = 0
-      let identifier = ObjectIdentifier(eventType)
+    var handledNotifications = 0
+    let identifier = ObjectIdentifier(eventType)
 
-      // Notify to direct subscribers
-      if let subscribers = subscribed[identifier] {
-        for subscriber in subscribers.lazy.filter ({ $0.isValid }) {
-          self.dispatchQueue.async {
-            subscriber.notify(closure: closure)
-          }
+    // Notify to direct subscribers
+    if let subscribers = subscribed[identifier] {
+      for subscriber in subscribers.lazy.filter ({ $0.isValid }) {
+        self.dispatchQueue.async {
+          subscriber.notify(closure: closure)
         }
-        handledNotifications += subscribers.count
       }
+      handledNotifications += subscribers.count
+    }
 
-      // Notify to indirect subscribers
-//      if let chains = self.chained[identifier] {
-//        for chain in chains.lazy.compactMap({ $0.inner as? EventNotifiable }) {
-//          handled += chain.notify(eventType, closure: closure) ? 1 : 0
-//        }
-//      }
-//      if (handled == 0) && options.contains(.warnUnhandled) {
-//        self.warnUnhandled(eventType)
-//      }
-      return handledNotifications > 0
+    // Notify to indirect subscribers
+    //      if let chains = self.chained[identifier] {
+    //        for chain in chains.lazy.compactMap({ $0.inner as? EventNotifiable }) {
+    //          handled += chain.notify(eventType, closure: closure) ? 1 : 0
+    //        }
+    //      }
+    //      if (handled == 0) && options.contains(.warnUnhandled) {
+    //        self.warnUnhandled(eventType)
+    //      }
+    return handledNotifications > 0
     //}
   }
 }
@@ -236,7 +237,7 @@ public protocol SubscriptionCancellable {
 
 extension EventBus {
 
-   private final class Subscription<T>: Hashable {
+  private final class Subscription<T>: Hashable {
 
     /// A subscription token to cancel a subscription.
     final class Token: SubscriptionCancellable {
@@ -270,15 +271,15 @@ extension EventBus {
     }
 
     fileprivate func notify<T>(closure: @escaping (T) -> ()) {
-        queue.async { [weak self] in
-          guard let `self` = self else { return }
+      queue.async { [weak self] in
+        guard let `self` = self else { return }
 
-          if let underlyngObject = self.underlyngObject {
-              closure(underlyngObject as! T)
-          } else {
-            self.token.cancel(completion: nil)
-          }
+        if let underlyngObject = self.underlyngObject {
+          closure(underlyngObject as! T)
+        } else {
+          self.token.cancel(completion: nil)
         }
+      }
     }
 
     internal static func == (lhs: Subscription, rhs: Subscription) -> Bool {
@@ -300,6 +301,7 @@ extension EventBus {
 }
 
 extension EventBus {
+
   internal func subscribers<T>(for eventType: T.Type) -> [AnyObject] {
     let identifier = ObjectIdentifier(eventType)
     if let subscribed = self.subscribed[identifier] {
@@ -308,7 +310,4 @@ extension EventBus {
     return []
   }
 
-//  internal var __subscribers: [ObjectIdentifier: SubscriberSet] {
-//    return subscribed
-//  }
 }
